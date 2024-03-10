7a36ba00d31276d85c5320343c9a26744c935b23
matiasbian <matiasezequielbian@gmail.com>
2024-03-10 15:20:58 -0300

    First commit

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RetroCloudSaving
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*const string userRoot = "HKEY_CURRENT_USER";
+            const string subkey = "RegistrySetValueExample";
+            const string keyName = userRoot + "\\" + subkey;
+
+            string value = "";
+            value = (string) Registry.GetValue(keyName, value, "NO VALUE");
+            MessageBox.Show(value);*/
+            SaveYuGiOhPowerOfChaos();
+            
+        }
+
+        private async void SaveYuGiOhPowerOfChaos ()
+        {
+            string game_path = @"C:\Program Files (x86)\KONAMI\Yu-Gi-Oh! Power of Chaos YUGI THE DESTINY\";
+            string file_path = "Region.dat";
+
+            await FTPRequest.Main(file_path, game_path);
+            MessageBox.Show("Task finalized");
+        }
+    }
+}
---
+++ FtpRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+
+public class FTPRequest
+{
+    const string FTP_URL = "ftp://ftp.dlptest.com/";
+    public static async Task Main(string filename, string game_path)
+    {
+        // Get the object used to communicate with the server.
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FTP_URL + filename);
+        request.Method = WebRequestMethods.Ftp.UploadFile;
+
+        // This example assumes the FTP site uses anonymous logon.
+        request.Credentials = new NetworkCredential("dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu");
+
+        // Copy the contents of the file to the request stream.
+        using (FileStream fileStream = File.Open(game_path + filename, FileMode.Open, FileAccess.Read))
+        {
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                await fileStream.CopyToAsync(requestStream);
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    Console.WriteLine($"Upload File Complete, status {response.StatusDescription}");
+                }
+            }
+        }
+    }
+}
